9d5b0edff774cd4795991c47bc87cb300c76f808
Beato Bongco <beatobongco@gmail.com>
2017-04-28 04:27:55 +0800

    Update docstring

--- quiz/problem_4.py
+++ quiz/problem_4.py
@@ -1,11 +1,14 @@
 def greedySum(L, s):
-  """ input: s, positive integer, what the sum should add up to
-         L, list of unique positive integers sorted in descending order
+  """
+    input: s, positive integer, what the sum should add up to
+           L, list of unique positive integers sorted in descending order
+
     Use the greedy approach where you find the largest multiplier for
     the largest value in L then for the second largest, and so on to
     solve the equation s = L[0]*m_0 + L[1]*m_1 + ... + L[n-1]*m_(n-1)
+
     return: the sum of the multipliers or "no solution" if greedy approach does
-        not yield a set of multipliers such that the equation sums to 's'
+            not yield a set of multipliers such that the equation sums to 's'
   """
 
   def get_sum(L, mults):
